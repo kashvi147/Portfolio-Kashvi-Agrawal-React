2e7cff74395430ac07761aa9dd3dd950aee9ba49
Kashvi Agrawal <kashvia.14@gmail.com>
2026-06-26 23:02:24 +0530

    Toggle, navbar rectified

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
+import { useEffect, useState } from "react";
 import "./Navbar.css";
 
 function Navbar() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    document.body.classList.toggle("light-mode", !darkMode);
+  }, [darkMode]);
   return (
     <nav className="navbar">
-      <ul className="nav-links">
+      
+
+  <div className="logo">
+    Kashvi
+  </div>
+
+  <button
+    className="menu-btn"
+    onClick={() => setMenuOpen(!menuOpen)}
+  >
+    {menuOpen ? "✕" : "☰"}
+  </button>
+
+  <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li><a href="#about">About</a></li>
         <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
@@ -16,9 +36,12 @@ function Navbar() {
         </li>
 
         <li>
-          <button className="theme-btn">
-            🌙
-          </button>
+          <button
+  className="theme-btn"
+  onClick={() => setDarkMode(!darkMode)}
+>
+  {darkMode ? "🌙" : "☀️"}
+</button>
         </li>
       </ul>
     </nav>
